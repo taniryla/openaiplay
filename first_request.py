116c18db2185efbe21f18ed5ed802295c985ce3a
Chako <ryland0000@gmail.com>
2023-08-30 20:37:00 -0700

    env

--- first_request.py
+++ first_request.py
@@ -1,4 +1,9 @@
 import openai
+from  dotenv import dotenv_values
+
+config = dotenv_values("./env")
+
+openai.api_key = config("OPENAI_API_KEY")
 
 openai.Completion.create(
     model="text-davinci-003",
