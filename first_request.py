49f9ce49d646dc3978025ce91c694b39b1c7cf6f
Chako <ryland0000@gmail.com>
2023-08-30 22:09:13 -0700

    variable

--- first_request.py
+++ first_request.py
@@ -5,9 +5,10 @@ config = dotenv_values("./env")
 
 openai.api_key = config("OPENAI_API_KEY")
 
-openai.Completion.create(
+response = openai.Completion.create(
     model="text-davinci-003",
-    prompt="The top 10 most populated cities are: ",
+    prompt="Generate a list of the best movies of all time: ",
     max_tokens=100
-
 )
+
+print(response["choices"][0]["text"])
